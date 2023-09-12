6bdb14b32f821fefcdd187b10bd4aa17109765f3
lononeiro <143819479+lononeiro@users.noreply.github.com>
2023-09-12 20:46:43 -0300

    Update cliente.py

--- socket,definitivo/cliente.py
+++ socket,definitivo/cliente.py
@@ -1,19 +1,19 @@
 import socket
 import threading
 
-nickname = input("escolha um nome: ")
+nickname = input("escolha um nome: ") #define nickname como o nome escolhido
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET: define o endereço ipv4, SOCK_STREAM: define o tipo de socket como TCP
+client.connect(('127.0.0.1', 55555)) #define o endereço IP
 
-def recieve():
+def recieve(): #função para fazer o esquema de receber mensagem
     while True:
         try:
-            message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                client.send(nickname.encode('utf-8'))
+            message = client.recv(1024).decode('utf-8') #recebe uma mensagem mandada pelo servidor
+            if message == 'NICK': #caso a mensagem recebida pelo servidor for nick ele envia o nickname
+                client.send(nickname.encode('utf-8')) #envia o nickname 
             else:
-                print(message)
+                print(message) #se nao receber nada diferente ele printa a mensagem recebida pelo servidor
         except:
             print('um erro aconteceu')
             client.close()
