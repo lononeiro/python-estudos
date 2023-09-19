7d7745ed55de58588947ec37db4c098beba98a11
lononeiro <143819479+lononeiro@users.noreply.github.com>
2023-09-19 18:13:29 -0300

    Update server.py

--- socket,definitivo/server.py
+++ socket,definitivo/server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import bcrypt
 
 host  = '127.0.0.1' #local host
 port = 55555
@@ -8,34 +9,36 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-comandos_lista = ['/cadastrar', '/emails', '/clientes']
+nome_administrador = 'administrador'
+
+#listas e dicionarios:
+usuarios_dicionario = {}
+comandos_exclamacao_dicionário = {}
+comandos_lista = ['/cadastrar', '/emails', '/clientes', '/mensagem, /emails_cadastrados']
+
 clients = []
 nicknames = []
-usuarios_dicionario = {}
+
 emails = ['lucassgs58@gmail.com', 'bimbimbambam@gmail.com', '123' ]
+emails_cadastrados = []
 senhas = ['123lucas', '123', '123']
 
 
-def add_dicionario(email, senha):
-    for email, senha in zip(emails, senhas):
-        usuarios_dicionario[email] = senha
-
-add_dicionario(emails, senhas)
-
-
-
+#funções:
 def comandos():
     while True:
         comando = input('ADMIN > ')
         if comando == '/cadastrar':
 
-
             email_novo = input('email: ')
             senha_nova = input('senha: ')
             emails.append(email_novo)
-            senhas.append(senha_nova)
+            senha_nova = senha_nova.encode('utf-8')
+            senha_encriptografada = bcrypt.hashpw(senha_nova, bcrypt.gensalt(10))
+
+            senhas.append(senha_encriptografada)
+            print(senhas)
             add_dicionario(email_novo, senha_nova)
-            print(usuarios_dicionario)
 
         elif comando == '/emails':
             print()
@@ -50,9 +53,43 @@ def comandos():
             print()
             for comando_lista in comandos_lista:
                 print(comando_lista)
+            print()
+
+        elif comando == '/mensagem':
+            print()
+            escrita_administrador = input('administrador: ')
+            escrita_administrador = str(escrita_administrador)
+            mensagem_administrador = f'{nome_administrador}: {escrita_administrador}'
+            mensagem_administrador = mensagem_administrador.encode('utf-8')
+            broadcast(mensagem_administrador)
+
+        elif comando == '/emails_cadastrados':
+             for email_cadastrado in emails_cadastrados:
+                  print(email_cadastrado)
+        
+        elif comando == '/comando_novo':
+             entrada = input('digite o que deverá ser escrito após !: ')
+             saida = input('digite o que o comando irá printar: ')
+             comandos_exclamacao_dicionário[entrada] = saida
+             print(comandos_exclamacao_dicionário)
 
 
 
+def add_dicionario(email, senha):
+    for email, senha in zip(emails, senhas):
+        usuarios_dicionario[email] = senha
+
+add_dicionario(emails, senhas)
+
+#função para cadastrar email e senha nova
+def cadastrar(email_novo, senha_nova):
+            email_novo = input('email: ')
+            senha_nova = input('senha: ')
+            emails.append(email_novo)
+            senhas.append(senha_nova)
+            add_dicionario(email_novo, senha_nova)
+
+#função para mandar a mensagem para todos os clientes
 def broadcast(message):
     for client in clients:
         client.send(message)
@@ -63,6 +100,7 @@ def handle(client):
         try:
             message = client.recv(1024)
             broadcast(message)  
+
         except:
             index = clients.index(client)
             clients.remove(client)
@@ -71,7 +109,7 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
-
+#função para receber e interpretar mensagens
 def recieve():
     while True:
         client, address = server.accept()
@@ -85,20 +123,25 @@ def recieve():
         email = client.recv(1024).decode('utf-8')
         email = str(email)
 
-        client.send('SENHA'.encode('utf-8'))
-        senha = client.recv(1024).decode('utf-8')
-        senha = str(senha)
- 
-
-
+        #verifica se o email recebido ja está logado
+        if email in emails_cadastrados:
+            client.send('CADASTRADO'.encode('utf-8'))
+        else:
+            client.send('SENHA'.encode('utf-8'))
+            senha = bcrypt.hashpw(client.recv(1024), bcrypt.gensalt(10))
+            senha = str(senha)
+            print(senha)
+        #verifica se o email digitado existe dentro da lista de emails
         if email in emails:
                     
-                    
+                    #verifica se a senha digitada está correta
                     senha_correspondente = usuarios_dicionario[email]
+                    #senha_correspondente = bcrypt.hashpw(senha_nova, senha_encriptografada)
                     if senha == senha_correspondente:
                         
                         nicknames.append(nickname)
                         clients.append(client)
+                        emails_cadastrados.append(email)
                     else:
                         client.send('PASS'.encode('utf-8'))
                         client.close()
@@ -109,19 +152,17 @@ def recieve():
             client.close()
             continue
 
-        
         print(f'nickname do cliente é {nickname}')
         broadcast(f'{nickname} entrou no chat!!!'.encode('utf-8'))
         client.send('conectado ao servidor'.encode('utf-8'))
 
-
+        #thread para rodar a função de lidar com as mensagens dentro da função de receber
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
+#thread para o servidor sempre poder dar comando
 admin_thread = threading.Thread(target=comandos)
 admin_thread.start()        
-
-        
 
 print('server está escutando')
 print('/comandos para ver os comandos.')
